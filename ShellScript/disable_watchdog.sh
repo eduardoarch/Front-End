7fb1e75ec0be37669a7e3ef4d65bf2a6c2387142
eduardoarch <eduardobckgnome@gmail.com>
2023-06-25 23:04:38 -0300

    Shell Script profissional

---
+++ ShellScript/disable_watchdog.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# disable_watchdog.sh - To disable watchdog timers (both software and hardware)
+
+# Site       : https://forum.endeavouros.com/t/slightly-slower-boot-and-shutdown/4466/5 
+#            : https://wiki.archlinux.org/title/Improving_performance_(Portugu%C3%AAs)
+# Autor      : Eduardo Rosa <NeonComputerArt@outlook.com>
+# Manutenção : Santos <NeonComputerArt@outlook.com>
+
+
+# ------------------------------------------------------------
+
+# Check if /etc/sysctl.d/disable_watchdog.conf exists
+if [ -f "/etc/sysctl.d/disable_watchdog.conf" ]; then
+    # Append the variable to the existing file
+    echo "kernel.nmi_watchdog = 0" | sudo tee -a /etc/sysctl.d/disable_watchdog.conf > /dev/null
+else
+    # Create a new file and add the variable
+    echo "kernel.nmi_watchdog = 0" | sudo tee /etc/sysctl.d/disable_watchdog.conf > /dev/null
+fi
+
+# Check if /etc/modprobe.d/watchdog.conf exists
+if [ -f "/etc/modprobe.d/watchdog.conf" ]; then
+    # Append the blacklist variables to the existing file
+    echo "iTCO_wdt blacklist" | sudo tee -a /etc/modprobe.d/watchdog.conf > /dev/null
+    echo "iTCO_vendor_support blacklist" | sudo tee -a /etc/modprobe.d/watchdog.conf > /dev/null
+else
+    # Create a new file and add the blacklist variables
+    echo "iTCO_wdt blacklist" | sudo tee /etc/modprobe.d/watchdog.conf > /dev/null
+    echo "iTCO_vendor_support blacklist" | sudo tee -a /etc/modprobe.d/watchdog.conf > /dev/null
+fi
+
+
+# chmod +x disable_watchdog.sh
