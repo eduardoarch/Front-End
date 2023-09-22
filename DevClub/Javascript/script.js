774905f800429c5567208068926954906406885e
eduardoarch <eduardobckgnome@gmail.com>
2023-09-21 21:45:47 -0300

    Eventos do JavaScript

---
+++ DevClub/Javascript/script.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("#main-input");
+
+function cliqueiNoBotao() {
+  console.log(input.value);
+}
+function digiteiNoInput() {
+  console.log("Digitei no input");
+}
---
+++ DevClub/Javascript/script1.js
@@ -0,0 +1,9 @@
+// Eventos
+
+const input = document.querySelector("#main-input");
+const select = document.querySelector("select");
+const button = document.querySelector(".main-button");
+
+select.addEventListener("change", function(){
+    console.log("Troquei de valor")
+});
