3982981f84d840dc1017497e2479b03267248668
eduardoarch <eduardobckgnome@gmail.com>
2024-02-26 19:27:58 -0300

    Adicionando a opcao -h (help)

--- ShellScript/Profissional/03-chaves/usuario.sh
+++ ShellScript/Profissional/03-chaves/usuario.sh
@@ -10,5 +10,19 @@
 # Eduardo, Fevereiro de 2024
 #
 
+MENSAGEM_USO="
+Uso: $0 [-h]
 
+-h Mostra esta tela de ajuda
+
+"
+
+# Tratamento das opcoes de linha de comando
+if test "$1" = "-h"
+then
+    echo "$MENSAGEM_USO"
+    exit 0
+fi
+
+# Processamento
 cut -d : -f 1,5 /etc/passwd | tr : \\t
