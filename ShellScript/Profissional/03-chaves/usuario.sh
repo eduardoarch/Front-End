ed8148e2c7ee9029f674d11db969c69f8cd3f6f0
eduardoarch <eduardobckgnome@gmail.com>
2024-02-26 19:19:40 -0300

    Adicionando a opcao -h (help)

--- ShellScript/Profissional/03-chaves/usuario.sh
+++ ShellScript/Profissional/03-chaves/usuario.sh
@@ -4,6 +4,9 @@
 # Mostra os logins e nomes de usuarios do sistema
 # Obs: Le dados do arquivo /etc/passwd
 #
+# Versao 1: Mostra usuarios e nomes separados por TAB
+# Versao 2: Adicionar suporte a opcao -h
+#
 # Eduardo, Fevereiro de 2024
 #
 
