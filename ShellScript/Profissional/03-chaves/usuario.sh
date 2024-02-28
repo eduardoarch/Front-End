56aa5e204e0ea9e4d7ebdfdf8ad437feefbd62bb
eduardoarch <eduardobckgnome@gmail.com>
2024-02-28 19:47:22 -0300

    Arrumando bug quando nao tem opcoes, basename no nome do programa, -V extraindo direto do cabecalho, adicionadas opcoes --help e --version

--- ShellScript/Profissional/03-chaves/usuario.sh
+++ ShellScript/Profissional/03-chaves/usuario.sh
@@ -7,6 +7,10 @@
 # Versao 1: Mostra usuarios e nomes separados por TAB
 # Versao 2: Adicionar suporte a opcao -h
 # Versao 3: Adicionado suporte a opcao -V e opcoes invalidas
+# Versao 4: Arrumando bug quando nao tem opcoes, basename no
+#			nome do programa, -V extraindo direto dos cabecalhos,
+#			adicionadas opcoes --help e --version
+#
 #
 # Eduardo, Fevereiro de 2024
 #
@@ -14,23 +18,27 @@
 MENSAGEM_USO="
 Uso: $0 [-h|-V]
 
--h Mostra esta tela de ajuda
--V Mostra a versao do programa
+-h, --help	Mostra esta tela de ajuda
+-V, --version	Mostra a versao do programa
 "
 
 # Tratamento das opcoes de linha de comando
 case "$1" in
--h)
+-h | --help)
 	echo "$MENSAGEM_USO"
 	exit 0
 	;;
--V)
-	echo $0 Versao 3
+-V | --version)
+	echo -n $(basename "$0")
+	# Extrai a versao diretamente dos cabecalhos do programa
+	grep '^# Versao ' "$0" | tail -1 | cut -d : -f 1 | tr -d \#
 	exit 0
 	;;
 *)
-	echo Opcao Invalida:$1
-	exit 1
+	if test -n "$1"; then
+		echo Opcao invalida: $1
+		exit 1
+	fi
 	;;
 esac
 
